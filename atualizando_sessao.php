58afd8d51826f188ea7745b5a06a8da7a43c9540
vek03 <vektromboni@gmail.com>
2022-06-02 16:05:41 -0300

    Arquivos dos Exemplos

---
+++ atualizando_sessao.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+
+$_SESSION["newsession"]=$value;
+
+$_SESSION["newsession"]=$updatedvalue;
+?>
---
+++ capturando_sessao.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+
+$_SESSION["newsession"]=$value;
+
+echo $_SESSION["newsession"];
+?>
